3c7d83587fbfdb80b2655e55d5c799ad9511efbf
tcoffee <projects@tcoffee.in>
2017-01-28 11:07:18 +0530

    scrollspy added
    scollspy

--- assets/javascripts/frontend.js
+++ assets/javascripts/frontend.js
@@ -1,4 +1,51 @@
+//scroll-spy
 
+var lastId,
+    topMenu = $(".pure-menu-list"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
+
+//go-to-top
 
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
@@ -13,33 +60,3 @@ $('#return-to-top').click(function () {      // When arrow is clicked
     }, 500);
 });
 
- $(function() {
-
-            var appendthis = ("<div class='modal-overlay js-modal-close'></div>");
-
-            $('a[data-modal-id]').click(function(e) {
-                e.preventDefault();
-                $("body").append(appendthis);
-                $(".modal-overlay").fadeTo(500, 0.7);
-                //$(".js-modalbox").fadeIn(500);
-                var modalBox = $(this).attr('data-modal-id');
-                $('#' + modalBox).fadeIn($(this).data());
-            });
-
-            $(".js-modal-close, .modal-overlay").click(function() {
-                $(".modal-box, .modal-overlay").fadeOut(500, function() {
-                    $(".modal-overlay").remove();
-                });
-
-            });
-
-            $(window).resize(function() {
-                $(".modal-box").css({
-                    top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
-                    left: ($(window).width() - $(".modal-box").outerWidth()) / 2
-                });
-            });
-
-            $(window).resize();
-
-        });
